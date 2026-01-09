3fd1d914a3162e04831982faa0df9ae18ad9036a
Kolawole Suleiman <kolawolesuleiman716@gmail.com>
2026-01-09 23:30:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name="cortex",
     version="0.1.0",
+    description="Lightweight Facebook Graph API wrapper",
+    author="CortexInvader",
+    author_email="cortexinvader@gmail.com",
+    url="https://github.com/cortexinvader001/cortex",
     packages=find_packages(),
+    python_requires=">=3.11",
     install_requires=[
         "Flask>=2.3.2",
         "requests>=2.32.0",
         "python-dotenv>=1.0.0"
     ],
-    python_requires=">=3.10",
-    url="https://github.com/cortexinvader/cortex",
-    author="CortexInvader",
-    description="Lightweight Facebook Graph API wrapper",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
 )
